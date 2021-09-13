767ba8dccaf136400495024c515dc3133668e6e5
Bob Gardner <bob.gardner@snapdocs.com>
2021-09-13 15:25:16 -0600

    fix test for all users

--- src/test/java/vn/zalopay/benchmark/core/BytesFieldContentsTest.java
+++ src/test/java/vn/zalopay/benchmark/core/BytesFieldContentsTest.java
@@ -4,15 +4,17 @@ import org.testng.Assert;
 import com.google.protobuf.ByteString;
 
 import org.testng.annotations.Test;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class BytesFieldContentsTest {
+    private static final Path CAT_PIC = Paths.get(System.getProperty("user.dir"), "src/test/resources/cat.jpeg");
 
     @Test
     public void testReadBytes() {
-        String path = "/Users/alanaklopstein/jmeter-grpc-request/src/test/resources/cat.jpeg";
         long offset = 0;
         int readLength = 10;
-        BytesFieldContents bfc = new BytesFieldContents(path, offset, readLength);
+        BytesFieldContents bfc = new BytesFieldContents(CAT_PIC.toString(), offset, readLength);
         ByteString read = bfc.readBytes();
         Assert.assertEquals(read, ByteString.copyFrom(new byte[]{(byte) 0xff, (byte) 0xd8, (byte) 0xff, (byte) 0xe0, (byte) 0x00, (byte) 0x10, (byte) 0x4a, (byte) 0x46, (byte) 0x49, (byte) 0x46}));
     }
